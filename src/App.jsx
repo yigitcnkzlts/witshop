5f60eec6582c1b1f85767f7b1ef6ea83c5cd3081
Yiğit Can Kızıltaş <seryigido23@gmail.com>
2026-01-25 02:39:11 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind ÇALIŞIYOR ✅
+      </h1>
+    </div>
+  );
+}
